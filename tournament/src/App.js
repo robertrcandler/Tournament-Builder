caf59abdbc827207c026300a9c10cc04584eb77e
Daniel Duran Jr <danielduranjr@Daniels-Air.lan>
2020-05-27 09:30:42 -0500

    navbar in homepage

--- tournament/src/App.js
+++ tournament/src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import '../node_modules/uikit/css/uikit.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar="mode: click">
+      {/* <!-- Logo --> */}
+      <div className="uk-navbar-left">
+        <a className="logo-text uk-navbar-item uk-logo" href="/">Tournament Finder</a>
+      </div>
+      {/* <!-- Menu --> */}
+      <div className="uk-navbar-right">
+        {/* <!-- Menu for desktop size screens --> */}
+        <ul className="desktopMenu-toggle uk-navbar-nav">
+          <li><a className="nav-link" href="/resources">Home</a></li>
+          <li><a className="nav-link" href="#">Game Search</a></li>
+          <div className="uk-navbar-item">
+            <a href="/login"><button className="uk-button uk-button-default" id="login">Log in</button></a>
+          </div>
+        </ul>
+        {/* <!-- Menu for mobile size screens --> */}
+        <ul className="mobileMenu-toggle uk-navbar-nav">
+          <li className="mobile-menu">
+            <div className="menu-icon uk-flex uk-flex-center" uk-icon="icon: menu"></div>
+            <div className="uk-navbar-dropdown">
+              <ul className="uk-nav uk-navbar-dropdown-nav">
+                <li><a href="/resources">Home</a></li>
+                <li><a href="#">Game Search</a></li>
+                <li className="uk-nav-divider"></li>
+                <li><a className="logoutLink" href="/login">Log in</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
     </div>
   );
 }
