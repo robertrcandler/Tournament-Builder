2f319a79c6265c305241783ac7c5276719a46985
Daniel Duran Jr <danielduranjr@Daniels-Air.lan>
2020-05-28 14:08:32 -0500

    added links to navbar

--- tournament/src/App.js
+++ tournament/src/App.js
@@ -1,45 +1,34 @@
-import React from 'react';
-import './App.css';
-// import '../node_modules/uikit/css/uikit.css'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './Home';
+import { Games } from './Games';
+import { Contact } from './Contact';
+import { NoMatch } from './NoMatch';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar';
+import { Jumbotron } from './components/Jumbotron';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar="mode: click">
-      {/* <!-- Logo --> */}
-      <div className="uk-navbar-left">
-        <a className="logo-text uk-navbar-item uk-logo" href="/">Tournament Finder</a>
-      </div>
-      {/* <!-- Menu --> */}
-      <div className="uk-navbar-right">
-        {/* <!-- Menu for desktop size screens --> */}
-        <ul className="desktopMenu-toggle uk-navbar-nav">
-          <li><a className="nav-link" href="/resources">Home</a></li>
-          <li><a className="nav-link" href="#">Game Search</a></li>
-          <div className="uk-navbar-item">
-            <a href="/login"><button className="uk-button uk-button-default" id="login">Log in</button></a>
-          </div>
-        </ul>
-        {/* <!-- Menu for mobile size screens --> */}
-        <ul className="mobileMenu-toggle uk-navbar-nav">
-          <li className="mobile-menu">
-            <div className="menu-icon uk-flex uk-flex-center" uk-icon="icon: menu"></div>
-            <div className="uk-navbar-dropdown">
-              <ul className="uk-nav uk-navbar-dropdown-nav">
-                <li><a href="/resources">Home</a></li>
-                <li><a href="#">Game Search</a></li>
-                <li className="uk-nav-divider"></li>
-                <li><a className="logoutLink" href="/login">Log in</a></li>
-              </ul>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      
+      <React.Fragment>
+        <Router>
+          <NavigationBar />
+          <Jumbotron />
+          <Layout>
+            <Switch>
+              
+              <Route exact path="/" component={Home} />
+              <Route path="/games" component={Games} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Layout>
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ tournament/src/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Home = () => (
+  <div>
+    <h2>Render Games Here</h2>
+    <p>scrolling Games Go Here</p>
+  </div>
+)
