f270792bba45bba3afca6dca99e8f4f972018945
Daniel Duran Jr <danielduranjr@Daniels-Air.attlocal.net>
2020-05-30 18:41:02 -0500

    setup axios call to api

--- tournament/src/Home.js
+++ tournament/src/Home.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Gamecard from "./components/Gamecard";
+import "./home.css";
+import axios from "axios"
 
 function Home() {
   const fakedata = [
@@ -54,30 +56,49 @@ function Home() {
       ratings_count: 3890,
     },
   ];
+
+  const [topGames, setTopGames] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating`,
+        { headers: { "User-Agent": "TournaFinder" } }
+      )
+      .then((response) => {
+        console.log(response)
+      })
+      .catch(function (e) {
+        console.log(e);
+      });
+  }, []);
+
   return (
     <div>
       <h2>Render Games Here</h2>
       <p>scrolling Games Go Here</p>
-      {fakedata.map((game) => (
-        <Gamecard
-          key={game.id}
-          id={game.id}
-          name={game.name}
-          backgroundImg={game.background_img}
-          rating={game.rating}
-          ratingTop={game.rating_top}
-          ratings1title={game.ratings[0].title}
-          ratings1percent={game.ratings[0].percent}
-          ratings1count={game.ratings[0].count}
-          ratings2title={game.ratings[1].title}
-          ratings2percent={game.ratings[1].percent}
-          ratings2count={game.ratings[1].count}
-          ratings3title={game.ratings[2].title}
-          ratings3percent={game.ratings[2].percent}
-          ratings3count={game.ratings[2].count}
-          ratingsCount={game.ratings_count}
-        />
-      ))}
+      <div className="gamecard-container">
+        {fakedata.map((game) => (
+          <Gamecard
+            key={game.id}
+            id={game.id}
+            name={game.name}
+            backgroundImg={game.background_img}
+            rating={game.rating}
+            ratingTop={game.rating_top}
+            ratings1title={game.ratings[0].title}
+            ratings1percent={game.ratings[0].percent}
+            ratings1count={game.ratings[0].count}
+            ratings2title={game.ratings[1].title}
+            ratings2percent={game.ratings[1].percent}
+            ratings2count={game.ratings[1].count}
+            ratings3title={game.ratings[2].title}
+            ratings3percent={game.ratings[2].percent}
+            ratings3count={game.ratings[2].count}
+            ratingsCount={game.ratings_count}
+          />
+        ))}
+      </div>
     </div>
   );
 }
