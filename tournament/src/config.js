38884ecad2fffcb8f861a5c5f885920998f028d6
Robert Candler <robert.candler.32@gmail.com>
2020-06-01 00:28:50 -0600

    working on setting up aws still

---
+++ tournament/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "tournament-builder-app"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_McJWVtRDQ",
+      APP_CLIENT_ID: "4b85dhrai3rjab3qft4cenlgh4",
+      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    }
+  };
