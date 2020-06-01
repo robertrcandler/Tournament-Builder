e3392663320a76d07d0e7ee775acc955e886c9f8
Robert Candler <robert.candler.32@gmail.com>
2020-06-01 07:13:37 -0600

    first form of login

--- tournament/src/login.js
+++ tournament/src/login.js
@@ -1,73 +1,52 @@
 import React from 'react';
 
 function login() {
-
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    // Name and Password from the register-form
+    var nm = document.getElementById('nm');
+    var pw = document.getElementById('pw');
+    
+    // storing input from register-form
+    function store() {
+        localStorage.setItem('nm', nm.value);
+        localStorage.setItem('pw', pw.value);
     }
-}
+
+    // check if stored data from register-form is equal to entered data in the   login-form
+    function check() {
+
+        // stored data from the register-form
+        var storedName = localStorage.getItem('nm');
+        var storedPw = localStorage.getItem('pw');
+
+        // entered data from the login-form
+        var userName = document.getElementById('userName');
+        var userPw = document.getElementById('userPw');
+
+        // check if stored data from register-form is equal to data from login form
+        if(userName.value == storedName && userPw.value == storedPw) {
+            alert('You are logged in.');
+        }else {
+            alert('ERROR.');
+        }
+    }
+
 
   return (
     <div>
-            <h2>Login</h2>
-            <button onclick="document.getElementById('login').style.display='block'" style="width:auto;">Login</button>
+        <form id="register-form"> 
+            <input id="nm" type="text" placeholder="Name" value=""/>
+            <input id="pw" type="password" placeholder="Password" value=""/>
+            <input id="rgstr_btn" type="submit" value="get Account" onClick="store()"/> 
+        </form>
 
-            <div id="login" class="modal">
-        
-                <form class="modal-content animate" action="/action_page.php" method="post">
-                    <div class="container">
-                        <label for="uname"><b>Username</b></label>
-                        <input type="text" placeholder="Enter Username" name="uname" required></input>
+        <form id="login-form"> 
+            <input id="userName" type="text" placeholder="Enter Username" value=""/>
+            <input id="userPw" type="password" placeholder="Enter Password" value=""/>
+            <input id="login_btn" type="submit" value="Login" onClick="check()"/> 
+       </form>
 
-                        <label for="psw"><b>Password</b></label>
-                        <input type="password" placeholder="Enter Password" name="psw" required></input>
-                        
-                        <button type="submit">Login</button>
-                    </div>
-                </form>
-            </div>
     </div>
   );
 }
 
 export default login;
-
-
-// import React from 'react'
-
-
-// // Get the modal
-// var modal = document.getElementById('id01');
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-
-// export const login = () => (
-//   <div>
-//         <h2>Login</h2>
-//         <button onclick="document.getElementById('login').style.display='block'" style="width:auto;">Login</button>
-
-//         <div id="login" class="modal">
-    
-//             <form class="modal-content animate" action="/action_page.php" method="post">
-//                 <div class="container">
-//                     <label for="uname"><b>Username</b></label>
-//                     <input type="text" placeholder="Enter Username" name="uname" required>
-
-//                     <label for="psw"><b>Password</b></label>
-//                     <input type="password" placeholder="Enter Password" name="psw" required>
-                    
-//                     <button type="submit">Login</button>
-//                 </div>
-//             </form>
-//         </div>
-//   </div>
-// )
