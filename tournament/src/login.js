2f0fa21fc48049a41e2f28d3f681259f419a197d
Robert Candler <robert.candler.32@gmail.com>
2020-06-01 02:24:09 -0600

    built front end of login page

--- tournament/src/login.js
+++ tournament/src/login.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
+function login() {
 
-// Get the modal
 var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
@@ -11,24 +11,63 @@ window.onclick = function(event) {
     }
 }
 
-export const login = () => (
-  <div>
-        <h2>Login</h2>
-        <button onclick="document.getElementById('login').style.display='block'" style="width:auto;">Login</button>
+  return (
+    <div>
+            <h2>Login</h2>
+            <button onclick="document.getElementById('login').style.display='block'" style="width:auto;">Login</button>
 
-        <div id="login" class="modal">
+            <div id="login" class="modal">
+        
+                <form class="modal-content animate" action="/action_page.php" method="post">
+                    <div class="container">
+                        <label for="uname"><b>Username</b></label>
+                        <input type="text" placeholder="Enter Username" name="uname" required></input>
+
+                        <label for="psw"><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="psw" required></input>
+                        
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+            </div>
+    </div>
+  );
+}
+
+export default login;
+
+
+// import React from 'react'
+
+
+// // Get the modal
+// var modal = document.getElementById('id01');
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+
+// export const login = () => (
+//   <div>
+//         <h2>Login</h2>
+//         <button onclick="document.getElementById('login').style.display='block'" style="width:auto;">Login</button>
+
+//         <div id="login" class="modal">
     
-            <form class="modal-content animate" action="/action_page.php" method="post">
-                <div class="container">
-                    <label for="uname"><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name="uname" required>
+//             <form class="modal-content animate" action="/action_page.php" method="post">
+//                 <div class="container">
+//                     <label for="uname"><b>Username</b></label>
+//                     <input type="text" placeholder="Enter Username" name="uname" required>
 
-                    <label for="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" required>
+//                     <label for="psw"><b>Password</b></label>
+//                     <input type="password" placeholder="Enter Password" name="psw" required>
                     
-                    <button type="submit">Login</button>
-                </div>
-            </form>
-        </div>
-  </div>
-)
+//                     <button type="submit">Login</button>
+//                 </div>
+//             </form>
+//         </div>
+//   </div>
+// )
